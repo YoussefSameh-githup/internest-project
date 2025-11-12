2228d561c58716f0f614f9ef4acbd99e85df3f62
Youssef Sameh <engineerthe973@gmail.com>
2025-11-12 13:51:30 +0200

    HOTFIX: Correct project name in wsgi.py

--- internest_app_project/wsgi.py
+++ internest_app_project/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Internest_Project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internest_app_project.settings')
 application = get_wsgi_application()
